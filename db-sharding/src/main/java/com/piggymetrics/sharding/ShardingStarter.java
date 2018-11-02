b9f2c543740e6b71bebc15f538eb78367750c220
zhoujian <zhoujian@newtank.cn>
2018-11-02 09:28:05 +0800

    auh-jwt分支完善

---
+++ db-sharding/src/main/java/com/piggymetrics/sharding/ShardingStarter.java
@@ -0,0 +1,12 @@
+package com.piggymetrics.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingStarter.class);
+    }
+}
